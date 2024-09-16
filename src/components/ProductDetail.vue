5788bb35436b02d3161c7ef00317bf90dd2e8bb0
0kasama <okaajyad@gmail.com>
2024-09-16 19:58:45 +0700

    feat(product): add test loading and error button

--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -19,6 +19,22 @@ const prevProduct = () => {
     id.value -= 1;
   }
 };
+
+// const test = ref(null);
+
+// const testLoading = () => {
+//   test.value = 'loading';
+//   setTimeout(() => {
+//     test.value = null;
+//   }, 2000);
+// };
+
+// const testError = () => {
+//   test.value = 'error';
+//   setTimeout(() => {
+//     test.value = null;
+//   }, 2000);
+// };
 </script>
 
 <template>
@@ -27,11 +43,15 @@ const prevProduct = () => {
   >
     <div class="bg-white flex w-full h-[30vh] absolute bottom-0 z-0"></div>
     <div
-      class="bg-white z-10 flex flex-col justify-center items-center w-3/4 h-2/3 p-10 rounded-xl shadow-xl"
+      class="bg-white z-10 flex flex-col justify-center items-center w-[70vw] h-[80vh] p-10 rounded-xl shadow-xl"
     >
+      <!-- <div v-if="test === 'loading'">Loading...</div>
+      <div v-else-if="test === 'error'" class="text-red-500">
+        Error: {{ error }}
+      </div> -->
       <div v-if="isLoading">Loading...</div>
-      <div v-if="error" class="text-red-500">Error: {{ error }}</div>
-      <div v-else-if="product">
+      <div v-else-if="error" class="text-red-500">Error: {{ error }}</div>
+      <div v-else="product">
         <div class="grid grid-cols-detail justify-between">
           <div class="flex justify-center items-center w-full">
             <img
@@ -44,9 +64,19 @@ const prevProduct = () => {
             <div class="flex flex-col justify-end">
               <h3>{{ product.title }}</h3>
             </div>
-            <div class="flex flex-row justify-between items-center my-2 border-b">
+            <div
+              class="flex flex-row justify-between items-center my-2 border-b"
+            >
               <h4>{{ product.category }}</h4>
-              <h4>{{ product.rating.rate }}</h4>
+              <div class="flex flex-row items-center gap-2">
+                <h4>{{ product.rating.rate }} / 5</h4>
+                <div
+                  v-for="index in 5"
+                  :key="index"
+                  class="rounded-full border border-primary w-5 h-5"
+                  :class="{ 'bg-primary': index < product.rating.rate }"
+                ></div>
+              </div>
             </div>
             <div class="flex items-self-start">
               <p>{{ product.description }}</p>
@@ -66,6 +96,14 @@ const prevProduct = () => {
                 Next
               </button>
             </div>
+            <!-- <div class="flex flex-row gap-4 mt-4">
+              <button @click="testLoading" class="btn btn-secondary">
+                Test Loading
+              </button>
+              <button @click="testError" class="btn btn-primary">
+                Test Error
+              </button>
+            </div> -->
           </div>
         </div>
       </div>
