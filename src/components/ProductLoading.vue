22a1f967544ffc26a2f1fda697d3a57aa4a9048b
0kasama <okaajyad@gmail.com>
2024-09-25 23:24:00 +0700

    feat(product): create loading skeleton

---
+++ src/components/ProductLoading.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="bg-main">
+    <div class="bg-main-under"></div>
+    <div class="card">
+      <div class="loading-container">
+        <div class="skeleton loading-img"></div>
+        <div class="loading-detail">
+            <div class="skeleton loading-title"></div>
+            <div class="skeleton loading-category"></div>
+            <div class="skeleton loading-description"></div>
+            <div class="skeleton loading-price"></div>
+            <div class="skeleton loading-button"></div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
